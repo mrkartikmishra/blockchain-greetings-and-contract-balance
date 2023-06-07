c1086717c4362197ecb766b9800554d8882b384a
Kartik Mishra <kartikmishra.km44@gmail.com>
2023-06-07 22:12:45 +0530

    react state added and button changehandler added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,87 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [greentingInput, setGreetingInput] = useState("");
+  const [depositeInput, setDepositeInput] = useState(0);
+  const [greenting, setGreeting] = useState("Kartik Mishra");
+  const [deposite, setDeposite] = useState(0.01);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const handleGreetingInputChange = (e) => {
+    setGreetingInput(e.target.value);
+  };
+
+  const handleDepositeInputChange = (e) => {
+    setDepositeInput(e.target.value);
+  };
+
+  const handleGreetingSubmit = (e) => {
+    e.preventDefault();
+    console.log(greentingInput);
+  };
+
+  const handleDepositeSubmit = (e) => {
+    e.preventDefault();
+    console.log(depositeInput);
+  };
+
   return (
     <div className="bg-black h-screen flex justify-between p-10 font-roboto">
       <div className="bg-gray w-1/2 rounded-l-md">
-        <form className="flex flex-col gap-3 flex-start p-10">
+        <form
+          onSubmit={handleGreetingSubmit}
+          className="flex flex-col gap-3 flex-start p-10"
+        >
           <input
             type="text"
             className="bg-transparent outline-none text-primary placeholder-primary border border-primary p-1 px-2 rounded-lg"
-            placeholder="Enter greetings"
+            placeholder="greetings"
+            value={greentingInput}
+            onChange={handleGreetingInputChange}
           />
-          <button className="bg-tertiary w-1/3 p-1 rounded-full text-primary font-semibold text-center my-2 mx-auto">
+          <button
+            type="submit"
+            className="bg-tertiary w-full p-1 rounded-full text-primary font-semibold text-center my-2 mx-auto"
+          >
             Change Greeting
           </button>
         </form>
-        <form className="flex flex-col gap-3 flex-start p-10">
+        <form
+          onSubmit={handleDepositeSubmit}
+          className="flex flex-col gap-3 flex-start p-10"
+        >
           <input
             type="number"
             className="bg-transparent outline-none text-primary placeholder-primary border border-primary p-1 px-2 rounded-lg"
             placeholder="0"
+            value={depositeInput}
+            onChange={handleDepositeInputChange}
           />
-          <button className="bg-tertiary w-1/4 p-1 rounded-full text-primary font-semibold text-center my-2 mx-auto">
+          <button
+            type="submit"
+            className="bg-tertiary w-full p-1 rounded-full text-primary font-semibold text-center my-2 mx-auto"
+          >
             Deposite
           </button>
         </form>
       </div>
-      <div className="bg-gray w-1/2 rounded-r-md right-0">
-        <div className="flex justify-end">
-          <div className="w-1/2 bg-tertiary shadow-xl p-10 rounded-lg font-semibold text-secondary">
-            <h3>Hello! Kartik</h3>
-            <p>Contract Balance: 0.001 ETH</p>
+      <div className="bg-gray w-1/2 md:w-1/2 rounded-r-md relative">
+        <div className="flex sm:justify-end">
+          <div className="w-full bg-tertiary shadow-xl p-10 rounded-lg font-semibold text-secondary">
+            <h3>{greenting}</h3>
+            <p>Contract Balance: {deposite} ETH</p>
           </div>
         </div>
+        <p className="absolute right-1 bottom-1 italic text-sewcondary">
+          {currentAccount ? (
+            `Connected: ${currentAccount}`
+          ) : (
+            <span className="text-red-700">Not connected</span>
+          )}
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default App;
